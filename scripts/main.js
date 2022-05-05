b02051f51bb16c5f0a4e0f577b35d8f0decae2ea
k-nautresurrect <kushignite@gmail.com>
2022-05-05 19:49:00 +0530

    Updated home page

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,11 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
   const visibility = primaryNav.getAttribute("data-visible");
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
   if (visibility === "false") {
     primaryNav.setAttribute("data-visible", true);
     navToggle.setAttribute("aria-expanded", true);
@@ -11,3 +16,22 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-expanded", false);
   }
 });
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".Dropbtn")) {
+    var dropdowns = document.getElementsByClassName("Dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+        console.log(openDropdown);
+      }
+    }
+  }
+};
